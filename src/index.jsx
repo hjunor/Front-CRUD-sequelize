613c116cb54adfce04950fe5aba16ad7d63578ad
Heberth Junor <hjunor@gmail.com>
2020-11-25 15:25:59 -0300

    Add: Added Sass

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import "./main.scss";
 import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
