cf27befa16140516f917dda74d53a7201ff7ed90
Heberth Junor <hjunor@gmail.com>
2021-01-21 09:33:52 -0300

    Add: screen Sign-in, new styles

--- src/screens/SignIn/index.jsx
+++ src/screens/SignIn/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { signIn } from "../../actions/AccountActions";
 import { getFormData } from "../../helpers/form";
-
+import {
+  Container,
+  Label,
+  WrapperLabel,
+  WrapperButton,
+  WrapperText,
+  Text,
+  TextHelp,
+  Links,
+} from "./styles";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 const SignIn = ({ account, signIn }) => {
   const submitHandler = (e) => {
     e.preventDefault();
@@ -15,40 +26,36 @@ const SignIn = ({ account, signIn }) => {
     return <Redirect to="/manage/links" />;
   }
   return (
-    <div className="container h-100 pt-5">
-      <h3>Sing In</h3>
-      <div className="d-flex flex-column h-100">
-        <form onSubmit={submitHandler}>
-          <div className="form-group">
-            <label htmlFor="">Email</label>
-            <input
-              placeholder="email@exemplo.com"
-              type="email"
-              className="form-control"
-              name="email"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="">Senha</label>
-            <input
-              placeholder="********"
-              type="password"
-              className="form-control"
-              name="password"
-            />
-          </div>
-          <div>
-            <button className="btn btn-primary btn-round" type="submit">
-              ok
-            </button>
-          </div>
-        </form>
-        <div className="container text-center fixed-botto pb-5">
-          <div className="text-muted">Don´t have an Account?</div>
-          <Link to="/sign-up">Sing Up</Link>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Text>Sing In</Text>
+      <form onSubmit={submitHandler}>
+        <WrapperLabel>
+          <Label>Email</Label>
+          <Input
+            placeholder="email@exemplo.com"
+            type="email"
+            name="email"
+            width="75vh"
+          />
+        </WrapperLabel>
+        <WrapperLabel>
+          <Label>Senha</Label>
+          <Input
+            placeholder="********"
+            type="password"
+            name="password"
+            width="75vh"
+          />
+        </WrapperLabel>
+        <WrapperButton>
+          <Button name="ok" color="#FFF" title="Ok" type="submit" />
+        </WrapperButton>
+      </form>
+      <WrapperText>
+        <TextHelp>Don´t have an Account?</TextHelp>
+        <Links to="/sign-up">Sing Up</Links>
+      </WrapperText>
+    </Container>
   );
 };
 
---
+++ src/screens/SignIn/styles.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const Label = styled.label`
+  color: ${(props) => props.theme.colors.text};
+  font-weight: bold;
+`;
+export const WrapperLabel = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Text = styled.p`
+  color: ${(props) => props.theme.colors.text};
+  font-weight: bold;
+  font-size: 4vh;
+  margin-top: 30px;
+`;
+export const WrapperButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+`;
+export const WrapperText = styled.div`
+  display: flex;
+  margin-top: 10px;
+`;
+
+export const TextHelp = styled.div`
+  color: ${(props) => props.theme.colors.text};
+  margin-right: 5px;
+`;
+
+export const Links = styled(Link)`
+  color: ${(props) => props.theme.colors.text};
+  font-weight: bold;
+  :hover {
+    color: ${(props) => props.theme.colors.secundary};
+  }
+`;
