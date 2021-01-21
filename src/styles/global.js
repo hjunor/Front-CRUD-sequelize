680da593dbcb101ab24f06cb88356d1ab7534ff0
Heberth Junor <hjunor@gmail.com>
2021-01-21 08:27:48 -0300

    Refactor: new styles global

---
+++ src/styles/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+  }
+  a{
+    text-decoration: none;
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+  }
+  body{
+    background: ${(props) => props.theme.colors.background};
+    font-size: 14px;
+    color: ${(props) => props.theme.colors.text};
+    font-family: sans-serif;
+    width: 100%;
+
+  }
+`;
