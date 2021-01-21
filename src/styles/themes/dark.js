be3fe88c7f01d9ea333c9fd3b137fdac8b900f78
Heberth Junor <hjunor@gmail.com>
2021-01-21 11:39:33 -0300

    Refactor: erros payload null

--- src/styles/themes/dark.js
+++ src/styles/themes/dark.js
@@ -1,12 +1,14 @@
-export default {
-  title: "dark",
-  colors: {
-    primary: "#333",
-    secundary: "#66306A",
-    background: "#222",
-    text: "#fff",
-    button: "#fff",
-    buttonText: "##66306A",
-    card: "#333",
-  },
-};
+export default function dark() {
+  return {
+    title: "dark",
+    colors: {
+      primary: "#333",
+      secundary: "#66306A",
+      background: "#222",
+      text: "#fff",
+      button: "#fff",
+      buttonText: "##66306A",
+      card: "#333",
+    },
+  };
+}
--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -1,12 +1,14 @@
-export default {
-  title: "light",
-  colors: {
-    primary: "#7E2E84",
-    secundary: "#D14081",
-    background: "#E4DEE4",
-    text: "#333",
-    button: "#66306A",
-    buttonText: "#fff",
-    card: "#fff",
-  },
-};
+export default function light() {
+  return {
+    title: "light",
+    colors: {
+      primary: "#7E2E84",
+      secundary: "#D14081",
+      background: "#E4DEE4",
+      text: "#333",
+      button: "#66306A",
+      buttonText: "#fff",
+      card: "#fff",
+    },
+  };
+}
